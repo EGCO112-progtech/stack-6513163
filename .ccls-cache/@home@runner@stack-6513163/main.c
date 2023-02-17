1d4a918d8a3eca31e3d219b4932dc0bdc1d3f7aa
6513163 <123149355+6513163@users.noreply.github.com>
2023-02-17 10:57:49 +0000

    work

--- .ccls-cache/@home@runner@stack-6513163/main.c
+++ .ccls-cache/@home@runner@stack-6513163/main.c
@@ -1,31 +1,75 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "node.h" 
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include "node.h"
 #include "stack.h"
+/*
+void push(NodePtr* top,int x);
+int pop(NodePtr* top);
+*/
 
-int main(int argc, char **argv) {
 
-  int i, N, j;
+
+int main(int argc, char **argv){
+ 
+  int i,N,j,error;
   Stack s;
   s.top=NULL;
   s.size=0;
 
-  for (i=1;i<argc;i++){
-    push(&s,atoi(argv[i])); //change string to int
+  for(i=1;i<argc;i++){
+    error=0;
+    for(j=0;j<strlen(argv[i]);j++){
+     switch(argv[i][j]){  
+       case '{':
+       case '[':  push(&s,argv[i][j]); break;
+     
+       case ']':  if(pop(&s)!='[')        
+                      error=1; break;
+       case '}':  if(pop(&s)!='{')        
+                      error=1; break;
+      }
+      if(error==1) break;
+    }
+    if(s.size>0){
+      printf("argv %d: Incorrect too many open parenthesis\n",i);
+      pop_all(&s);
+    }
+    else if(error==0) printf("argv %d: Correct\n",i);
+      else printf("argv %d : Incorrect\n",i);
   }
- pop_all(&s);
-  
-  
 
+  //pop_all(&s);
+ 
   /*
-  Stack s;
-  printf("Checking the parentheses in argv arguments\n");
-   for(i=1;i<argc;i++){
+  push(&top,5);
+  printf("%d\n",pop(&top));
+  push(&top,7);
+  push(&top,8);
+  printf("%d\n",pop(&top));
+  printf("%d\n",pop(&top));
+  printf("%d\n",pop(&top));
+*/
+ 
+ 
+ 
 
-      for(j=0;j<strlen(argv[i]);j++){
-        Use stack to help with the parentheses \
+ /*
+ Stack s;
+ printf("Checking the parentheses in argv arguments\n");
+  for(i=1;i<argc;i++){
+   
+     for(j=0;j<strlen(argv[i]);j++){
+       Use stack to help with the parentheses
+
+
+   
+
+
  */
 
-  return 0;
+
+
+
+   return 0;
 }
